4f08317bbcd695957fecadcd2320925810034b22
kousyougi <43228565+kousyougi@users.noreply.github.com>
2021-06-28 21:37:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add parted
+svn co https://github.com/kousyougi/openwrt/trunk/parted package/parted
